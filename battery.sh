d77eb0052f8d0dcd55fb87edab4206e36c2ce498
anuragpeshne <anurag.peshne@gmail.com>
2018-03-05 17:27:02 -0500

    added support for OSX

--- battery.sh
+++ battery.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 function battery {
-  PERCENTAGE=$(upower -i $(upower -e | grep '/battery') | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//)
-  printf "%s" "$PERCENTAGE%"
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    ISTATS=$(istats)
+    PERCENTAGE=$(echo $ISTATS | egrep -o "Current charge.* Max" | egrep -o "[0-9]*%")
+    printf "%s" "$PERCENTAGE"
+  elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    PERCENTAGE=$(upower -i $(upower -e | grep '/battery') | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//)
+    printf "%s" "$PERCENTAGE%"
+  fi
 }
--- cpu-temp.sh
+++ cpu-temp.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 function cpu-temp {
-  TEMP=$(cat /sys/class/thermal/thermal_zone*/temp)
-  for it in $TEMP; do printf "%s°C" $(( $it / 1000 )); done
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    ISTATS=$(istats)
+    TEMP=$(echo $ISTATS | egrep -o "temp: [0-9.]*°C" | cut -c 6-)
+    printf "%s" $TEMP;
+  elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    TEMP=$(cat /sys/class/thermal/thermal_zone*/temp)
+    for it in $TEMP; do printf "%s°C" $(( $it / 1000 )); done
+  fi
 }
