261913f89a66615c799cd76e0bf4485b52ee07b4
Anurag Peshne <anurag.peshne@gmail.com>
2018-03-05 14:21:31 -0500

    better notation for hw temperature

--- cpu-temp.sh
+++ cpu-temp.sh
@@ -2,5 +2,5 @@
 
 function cpu-temp {
   TEMP=$(cat /sys/class/thermal/thermal_zone*/temp)
-  for it in $TEMP; do printf "%sC " $(( $it / 1000 )); done
+  for it in $TEMP; do printf "%s°C" $(( $it / 1000 )); done
 }
